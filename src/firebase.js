c8c51600997aa037caa6efe87bb6ba76d1a9be21
gega0397 <g_kandelaki@cu.edu.ge>
2021-08-21 15:39:16 +0400

    add firebase file to src

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBn6cM7abcxlhIfFvA9IQIAqocXPx6kEw8",
+    authDomain: "clone-ae0b8.firebaseapp.com",
+    projectId: "clone-ae0b8",
+    storageBucket: "clone-ae0b8.appspot.com",
+    messagingSenderId: "578328873367",
+    appId: "1:578328873367:web:0d52fb09872122e1429cd2",
+    measurementId: "G-B08JTEGX8R"
+  };
