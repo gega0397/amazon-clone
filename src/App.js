19e0813664e1102ad1c0654dfa4698dafd0dd285
gega0397 <g_kandelaki@cu.edu.ge>
2021-08-23 02:18:10 +0400

    added Router functionality + checkout route in App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,34 @@ import React from "react";
 import './App.css';
 import Header from './Header';
 import Home from './Home'
+import { BrowserRouter as Router, Switch, Route}
+from "react-router-dom";
 
 function App() {
   return (
     //BEM
-    <div className="app">
-        {/* Header */}
-        <Header />
-        {/* Home */}
-        <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          
+          <Route path="/checkout">
+            {/* Header */}
+            <Header />
+            {/* Home */}
+            <p>Hello</p>
+            <h1>Hello Header</h1>
+          </Route>
+
+          <Route path="/">
+            {/* Header */}
+            <Header />
+            {/* Home */}
+            <Home />
+          </Route>
+
+        </Switch> 
+      </div>
+    </Router>
   );
 }
 
